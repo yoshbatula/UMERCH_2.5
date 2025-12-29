c37b5415dca3c1600b4b826cf2f564598af7bd37
yoshbatula <yoshbatula2@gmail.com>
2025-12-29 17:38:37 +0800

    initial ui for products

--- resources/js/Pages/User-side/Shop.jsx
+++ resources/js/Pages/User-side/Shop.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import Navbar from '../../components/Navbar';
+import Background from '@images/BackgroundModel.png'
 export default function Shop() {
     return (
         <>
             <Navbar/>
-            
+            <div className='bg-[#F6F6F6] flex flex-col justify-center'>
+                <div className='w-full h-48 sm:h-56 md:h-64 lg:h-72 overflow-hidden'>
+                    <img src={Background} alt="Background" className='w-full h-full object-cover'/>
+                </div>
+            </div>
         </>
     );
 }
