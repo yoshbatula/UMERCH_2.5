8ad5c0117b51b21dfda8ae629a8e0b7cdb978f50
yoshbatula <yoshbatula2@gmail.com>
2025-12-29 15:27:26 +0800

    shop features

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return inertia('Login');
 });
+
+Route::get('/Shop', function () {
+    return inertia('User-side/Shop');
+})->name('shop');
