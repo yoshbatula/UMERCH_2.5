f7122b5f9f37ece9ea227a8b3020b25847f614bf
yoshbatula <yoshbatula2@gmail.com>
2025-12-29 14:10:01 +0800

    done for the Hero Section

--- resources/js/components/Hero.jsx
+++ resources/js/components/Hero.jsx
@@ -5,17 +5,19 @@ export default function Hero() {
         <div className='flex flex-col justify-center items-center'>
             <div className='relative w-full'>
                 <img src={HeroBg} alt="Hero Background" className='object-cover w-full h-[30rem]'/>
-                <div className='absolute inset-0 bg-black opacity-58'></div>
-                <div className='absolute inset-0 bg-amber-300 opacity-20'></div>
+                <div className='absolute inset-0 bg-black opacity-60'></div>
+                <div className='absolute inset-0 bg-amber-300 opacity-16'></div>
                 <div className='absolute inset-0 flex flex-col justify-center items-center'>
-                    <div className='flex flex-col text-start text-white transform translate-x-[120px]'>
+                    <div className='flex flex-col text-start text-white transform translate-x-[230px]'>
                         <p className='text-[16px] font-semibold mb-3'>EXPLORE</p>
-                        <div className='text-[48px] font-semibold font-coromont'>
+                        <div className='text-[56px] font-semibold font-CormorantGaramond' style={{fontFamily: "'Cormorant Garamond', serif"}}>
                             <h1>Elevate your fashion, embrace</h1>
                             <h1>UM Timeless Style!</h1>
                         </div>
-                        <p>Explore our collections today and experience the joy of fashion.</p>
-                        <p>Shop now for the ultimate casual style!</p>
+                        <div className='text-[14px] font-semibold'>
+                            <p>Explore our collections today and experience the joy of fashion.</p>
+                            <p>Shop now for the ultimate casual style!</p>
+                        </div>
                     </div>
                 </div>
             </div>
