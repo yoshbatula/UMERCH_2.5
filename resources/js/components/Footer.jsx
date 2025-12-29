fb949475c064ce39c215c50ccd216f7eca943094
yoshbatula <yoshbatula2@gmail.com>
2025-12-29 15:04:00 +0800

    Finally done for footer

--- resources/js/components/Footer.jsx
+++ resources/js/components/Footer.jsx
@@ -9,8 +9,9 @@ import Twitter from '@images/Twitter.svg';
 import Youtube from '@images/Youtube.svg';
 export default function Footer() {
     return (
-        <div className="p-6 bg-[#F6F6F6] flex flex-row justify-center items-center">
-            <div className="flex flex-row gap-20">
+        <>
+        <div className="p-8 bg-[#F6F6F6] flex flex-col justify-center items-center">
+            <div className="flex flex-row gap-30">
                 <div className="flex flex-col">
                     <h1 className="text-[20px]">Menu</h1>
                     <div className="mt-1 flex flex-col text-[#727272] gap-1">
@@ -28,14 +29,42 @@ export default function Footer() {
                 </div>
                 <div className="flex flex-col">
                     <h1 className="text-[20px]">Contact Us</h1>
-                    <div className="mt-1 flex flex-row text-[#727272] gap-1">
+                    <div className="mt-1 flex flex-row text-[#727272] gap-2">
                         <img src={Location} alt="Location-Icon"/>
-                        <div>
-                            <p>Bolton St, Poblacion District, </p>
+                        <div className="flex flex-col text-start">
+                            <p>Bolton St, Poblacion District,</p>
+                            <p>Davao City, 8000 Davao del Sur</p>
                         </div>
+                    </div>
+                    <div className="mt-1 flex flex-row text-[#727272] gap-2">
+                        <img src={Email} alt="Email-Icon"/>
+                        <div className="flex flex-col text-start">
+                            <p>demo@example.com</p>
+                            <p>info@example.com</p>
+                        </div>
+                    </div>
+                    <div className="mt-1 flex flex-row text-[#727272] gap-2">
+                        <img src={Phone} alt="Phone-Icon"/>
+                        <div className="flex flex-col text-start">
+                            <p>01234567890</p>
+                            <p>01234567890</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="flex flex-col">
+                    <h1 className="text-[20px]">Social Media</h1>
+                    <div className="mt-1 flex flex-row text-[#727272] gap-2">
+                        <Link href="#"><img src={Facebook} alt="Facebook-Icon" /></Link>
+                        <Link href="#"><img src={Instagram} alt="Instagram-Icon" /></Link>
+                        <Link href="#"><img src={Youtube} alt="Youtube-Icon" /></Link>
+                        <Link href="#"><img src={Twitter} alt="Twitter-Icon" /></Link>
                     </div>
                 </div>
             </div>
         </div>
+        <div className="p-6 bg-white flex justify-center items-center">
+            <h1 className="text-[#727272] font-medium">Copyright © 2025 UMerch | Powered by Univebersity of Mindanao</h1>
+        </div>
+        </>
     );
 }
