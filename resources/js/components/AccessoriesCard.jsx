ad9e99be68342f46cbf9c204b702ee467b0d7d99
yoshbatula <yoshbatula2@gmail.com>
2025-12-29 13:40:09 +0800

    done for the FeatureSection

--- resources/js/components/AccessoriesCard.jsx
+++ resources/js/components/AccessoriesCard.jsx
@@ -31,7 +31,7 @@ export default function AccessoriesCard() {
                     </div>
                 </div>
             </div>
-            <div className="mt-10 flex justify-center items-center">
+            <div className="flex justify-center items-center p-10">
                 <div className="bg-[#9C0306] flex justify-center items-center w-60 h-10 rounded-[20px] hover:cursor-pointer">
                     <button className="text-white text-[16px] font-semibold">SEE MORE ACCESSORIES</button>
                 </div>
--- resources/js/components/FeatureSection.jsx
+++ resources/js/components/FeatureSection.jsx
@@ -1,10 +1,40 @@
 import React from 'react';
-
+import CampusDelivery from '@images/CampusDelivery.svg';
+import BestQuality from '@images/BestQuality.svg'
+import BestOffer from '@images/BestOffer.svg'
+import SecurePayment from '@images/SecurePayment.svg'
 export default function FeatureSection() {
     return(
         <div className='bg-white'>
-            <div>
-                <h1>HELLO</h1>
+            <div className='p-10 flex flex-row justify-center items-center gap-30'>
+                <div className='flex flex-col text-center items-center leading-tight'>
+                    <img src={CampusDelivery} alt="Campus Delivery" className='w-20'/>
+                    <h1 className='font-medium text-[20px] mt-3'>Campus Delivery</h1>
+                    <p className='mt-5'>Lorem ipsum dolor sit amet</p>
+                    <p>consectetur. Eget sed sapien</p>
+                    <p>quisque et suspendisse.</p>
+                </div>
+                <div className='flex flex-col text-center items-center leading-tight'>
+                    <img src={BestQuality} alt="Campus Delivery" className='w-20'/>
+                    <h1 className='font-medium text-[20px] mt-3'>Best Quality</h1>
+                    <p className='mt-5'>Lorem ipsum dolor sit amet </p>
+                    <p>consectetur. Eget sed sapien</p>
+                    <p>quisque et suspendisse.</p>
+                </div>
+                <div className='flex flex-col text-center items-center leading-tight'>
+                    <img src={BestOffer} alt="Campus Delivery" className='w-20'/>
+                    <h1 className='font-medium text-[20px] mt-3'>Best Offer</h1>
+                    <p className='mt-5'>Lorem ipsum dolor sit amet</p>
+                    <p>consectetur. Eget sed sapien</p>
+                    <p>quisque et suspendisse.</p>
+                </div>
+                <div className='flex flex-col text-center items-center leading-tight'>
+                    <img src={SecurePayment} alt="Campus Delivery" className='w-20'/>
+                    <h1 className='font-medium text-[20px] mt-3'>Best Offer</h1>
+                    <p className='mt-5'>Lorem ipsum dolor sit amet</p>
+                    <p>consectetur. Eget sed sapien</p>
+                    <p>quisque et suspendisse.</p>
+                </div> 
             </div>
         </div>
     );
