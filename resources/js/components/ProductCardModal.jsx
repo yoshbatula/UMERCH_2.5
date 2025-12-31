35125f3a27cbc93b0104382de04b5ea0a8a5b720
yoshbatula <yoshbatula2@gmail.com>
2025-12-31 14:59:12 +0800

    finally done

--- resources/js/components/ProductCardModal.jsx
+++ resources/js/components/ProductCardModal.jsx
@@ -5,15 +5,20 @@ export default function ProductCardModal({ isOpen, onClose }) {
 
     return (
         <div
-            className='fixed inset-0 z-50 flex  backdrop-blur-xs bg-white/5'
+            className='fixed inset-0 z-50 flex justify-center items-center  backdrop-blur-xs bg-white/5'
             onClick={onClose}
         >
             <div
-                className="bg-white p-6 rounded shadow-lg relative"
+                className="bg-white p-2 rounded shadow-lg relative w-180 h-100"
                 onClick={e => e.stopPropagation()}
             >
-                <div className='flex flex-row'>
-                    <img src={Tumbler} alt="Tumbler" className='w-10'/>
+                <div className='flex flex-row p-8 gap-10'>
+                    <img src={Tumbler} alt="Tumbler" className='w-80'/>
+                    <div className='flex flex-col'>
+                        <div>
+                            <h2>Wooden</h2>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
