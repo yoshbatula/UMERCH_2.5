98656c2d1489701532158c693bc869ee1a8486c3
yoshbatula <yoshbatula2@gmail.com>
2026-01-03 13:04:07 +0800

    initial commit

--- resources/js/components/modals/ProductAccessoriesModal.jsx
+++ resources/js/components/modals/ProductAccessoriesModal.jsx
@@ -69,7 +69,6 @@ export default function ProductCardModal({ isOpen, onClose }) {
                     </div>
                 </div>
             </div>
-            {/* initial commit */}
         </div>
     );
 }
